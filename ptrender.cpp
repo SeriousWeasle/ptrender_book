34d0a7ca837d318944c1795152992528d6ad11f9
SeriousWeasle <seriousweasle@gmail.com>
2019-07-03 15:43:17 +0200

    finished random scene rendering

--- ptrender.cpp
+++ ptrender.cpp
@@ -1,38 +1,83 @@
 #include <iostream>
 #include <fstream>
-#include "sphere.h"
-#include "hitable_list.h"
 #include "float.h"
-#include "camera.h"
-#include "drand.h"
+#include "./headers/drand.h"
+#include "./headers/sphere.h"
+#include "./headers/hitable_list.h"
+#include "./headers/material.h"
+#include "./headers/camera.h"
 
 using namespace std;
 
-vec3 color(const ray& r, hitable *world) {
+vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation*color(scattered,world,depth+1);
+        }
+        else {
+            return vec3(0,0,0);
+        }
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y()+1.0);
+        float t = 0.5*(unit_direction.y() + 1.0);
         return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
     }
 }
+
+hitable *random_scene() {
+        int n = 500;
+        hitable **list = new hitable*[n+1];
+        list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));
+        int i = 1;
+        for (int a = -11; a < 11; a++) {
+            for (int b = -11; b < 11; b++) {
+                float choose_mat = drand48();
+                vec3 center(a+0.9*drand48(),0.2, b+0.9*drand48());
+                if((center-vec3(4,0.2,0)).length() > 0.9) {
+                    if (choose_mat < 0.8) {
+                        list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                    }
+                    else if (choose_mat <0.95) {
+                        list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1+drand48()), 0.5*(1+drand48()), 0.5*(1+drand48())), 0.5*drand48()));
+                    }
+                    else {
+                        list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                    }
+                }
+            }
+        }
+        list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+        list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+        list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+        return new hitable_list(list, i);
+    }
 
 int main()
 {
     ofstream outfile;
     outfile.open ("output.ppm");
-    int nx = 1600;
-    int ny = 800;
-    int ns = 100;
+    int nx = 200;
+    int ny = 100;
+    int ns = 10;
     outfile << "P3\n" << nx << " " << ny << "\n255\n";
-    hitable *list[2];
-    list[0] = new sphere(vec3(0,0,-1), 0.5);
-    list[1] = new sphere(vec3(0,-1000.5,-1),1000);
-    hitable *world = new hitable_list(list,2);
-    camera cam;
+    hitable *list[5];
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
+    list[1] = new sphere(vec3(0,-100.5,-1),100, new lambertian(vec3(0.4, 0, 0.8)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    hitable *world = new hitable_list(list,5);
+    world = random_scene();
+    vec3 lookfrom(20,7,15);
+    vec3 lookat (0,0,-1);
+    float dist_to_focus = (lookfrom-lookat).length();
+    float aperture = 0.2;
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
     for (int j = ny - 1; j >= 0; j--){
         for (int i = 0; i < nx; i++) {
             vec3 col(0, 0, 0);
@@ -41,13 +86,21 @@ int main()
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col/= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
             outfile << ir << " " << ig << " " << ib << "\n";
+            float currpx = i;
+            float totalpx = nx;
+            float totalrows = ny;
+            float currow = j;
+            float pixelpercent = (currpx*100/totalpx)/totalrows;
+            float renderpercent = ((totalrows-currow-1)+pixelpercent)*100.0/totalrows;
+            cout << "Render is at " << renderpercent << "%\n";
         }
     }
     outfile.close();
